08c2c0c2f4ef174662aa0a18cf117229fa663c23
chocoding <galmeagi2@naver.com>
2025-03-14 02:46:14 +0900

    로그인 관련 로직 생성

---
+++ src/main/java/com/cos/security1/auth/PrincipalDetails.java
@@ -0,0 +1,87 @@
+package com.cos.security1.auth;
+
+import com.cos.security1.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * 시큐리티가 /login 주소 요청이 오면 낚아채서 로그인을 진행시킨다.
+ * 로그인이 완료가 되면 시큐리티 session을 만들어준다.
+ * 세션 공간은 HttpSession과 동일한데, 시큐리티가 그 안에 시큐리티 세션 공간을 따로 분리한다.
+ * 'Security ContextHolder'라는 키값에 세션 정보를 저장한다.
+ * 이 때 시큐리티가 가지고 있는 세션에 들어갈 수 있는 오브젝트는 정해져 있다. => Authentication 타입의 객체만 들어갈 수 있다.
+ * 그리고 Authentication 안에 User 정보가 있어야 된다.
+ * 이 User 오브젝트의 타입도 정해져 있다. => UserDetails 타입의 객체만 들어갈 수 있다.
+ * 즉 시큐리티 세션에는 Authentication 타입만 저장될 수 있고, Authentication 안에는 UserDetails 타입만 저장될 수 있다.
+ * Security Session <= Type(Authentication) <= Type(UserDetails)
+ *
+ * UserDetails 만들었으면, Authentication도 만들어야 한다.
+ */
+
+public class PrincipalDetails implements UserDetails { // UserDetails를 구현함으로써, PrincipalDetails 타입은 Authentication 안에 저장될 수 있다.
+
+    private User user; // 콤포지션 (이게 뭐지?)
+
+    public PrincipalDetails(User user) {
+        this.user = user;
+    }
+
+    // 해당 User의 권한을 리턴하는 곳 (권한은 유저의 role을 뜻한다.)
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collect = new ArrayList<>();
+        // user의 role은 String 타입이기 때문에 GrantedAuthority 타입으로 변환해야 한다.
+        collect.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return user.getRole();
+            }
+        });
+        return collect;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    // 계정이 만료됐는지 체크
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    // 계정이 잠겼는지 체크
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    // 계정의 비밀번호가 유효기간이 지났는지 체크
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    // 계정이 활성화되어 있는지 체크
+    @Override
+    public boolean isEnabled() {
+        /**
+         * 언제 이런 것들을 사용하냐
+         * ex) 우리 서비스에 회원이 1년동안 로그인을 안 하면, 휴면 계정으로 전환하고자 할 때 사용
+         * 위처럼 서비스를 만든다면, User 엔티티에 lastLoginDate같은 필드를 넣어서
+         * 현재 날짜 = user.getLastLoginDate > 1년
+         * 이면 false를 반환하도록 로직을 추가하면 된다.
+         */
+        return true;
+    }
+
+}
---
+++ src/main/java/com/cos/security1/auth/PrincipalDetailsService.java
@@ -0,0 +1,49 @@
+package com.cos.security1.auth;
+
+import com.cos.security1.model.User;
+import com.cos.security1.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * Authentication 객체 만들어주는 Service
+ * 얘는 언제 발동되냐
+ * 시큐리티 설정(SecurityConfig)에서 loginProcessingUrl("/login")을 설정했을 떄,
+ * /login 요청이 오면, 자동으로 UserDetailsService 타입으로 IoC되어 있는 loadUserByUsername 함수가 실행된다.
+ * 이건 그냥 규칙이기 때문에 꼭 UserDetailsService 타입으로 만들어야 한다. 그래야 해당 타입으로 되어있는 loadUserByUsername이 호출된다.
+ */
+
+/**
+ * 로그인 폼에서 로그인 버튼을 누르면 /login url로 POST 요청이 들어온다. (securityConfig에서 설정)
+ * 그럼 시큐리티가 그걸 낚아채고, Bean 컨테이너에서 UserDetailsService 타입의 빈을 찾는다. (여기서는 PrincipalDetailsService)
+ * 해당 빈이 있으면 바로 loadUserByUsername 메서드를 호출한다. 이 떄 폼에서 작성된 username을 가져온다.
+ */
+@Service
+@RequiredArgsConstructor
+public class PrincipalDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    /**
+     * 파라미터로 받는 username은 loginForm에서 날아오는 username이기 때문에
+     * username을 받는 영역의 name을 꼭 username으로 맞춰줘야 매칭이 된다.
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        // 넘어온 username으로 회원이 존재하는지 확인
+        User userEntity = userRepository.findByUsername(username);
+        if (userEntity != null) {
+            return new PrincipalDetails(userEntity); // principalDetails는 UserDetails를 상속받기 때문에 반환 가능
+            /**
+             * 유저 객체를 담아서 리턴하면 이 UserDetails 객체는 Authentication 내부에 들어가게 된다. Authentication(UserDetails)
+             * 그리고 그 Authentication 객체는 시큐리티 session에 들어가게 된다. 시큐리티 session(Authentication(UserDetails))
+             * 즉 PrincipalDetails를 리턴하면 자동으로 시큐리티 세션이 초기화되는 것이다. (loadUserByUsername 메서드가 알아서 다 해주는 것)
+             */
+        }
+        return null;
+    }
+}
