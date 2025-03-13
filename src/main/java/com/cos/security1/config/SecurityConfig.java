427e8d5b975b36801578447d2d47e66a06f7fdac
chocoding <galmeagi2@naver.com>
2025-03-14 02:43:27 +0900

    폼 로그인 설정 추가

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.cos.security1.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
@@ -10,6 +12,13 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터 체인에 등록되도록 한다.
+@EnableMethodSecurity(securedEnabled = true, prePostEnabled = true) // secured 어노테이션 활성화(default : prePostEnabled = true)
+/**
+ * @EnableMethodSecurity
+ * @EnableGlobalMethodSecurity 대체
+ * securedEnabled = true : @Secured 어노테이션 활성화
+ * prePostEnabled = true : @PreAuthorize, @PostAuthorize 어노테이션 활성화
+ */
 public class SecurityConfig {
 
     @Bean
@@ -20,14 +29,22 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(CsrfConfigurer::disable);
-        http    .authorizeHttpRequests(auth -> auth
-                                .requestMatchers("/user/**").authenticated() // /user로 들어오는 경우에는 인증 필요
-                                .requestMatchers("/manager/**").hasAnyRole("ADMIN", "MANAGER") // /manager로 들어오는 경우에는 ADMIN이나 MANAGER 권한 필요
-                                .requestMatchers("/admin/**").hasAnyRole("ADMIN") // /admin으로 들어오는 경우에는 ADMIN 권한 필요
-                                .anyRequest().permitAll()) // 이외의 접근은 모두 허용)
-                .formLogin(formLogin -> // 권한이 필요하다고 설정한 /user/**, /manager/**, /admin/**에 대해 /login으로 이동하도록 설정
-                        formLogin.loginPage("/loginForm"));
-
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/user/**").authenticated() // /user로 들어오는 경우에는 인증 필요
+                        .requestMatchers("/manager/**").hasAnyRole("ADMIN", "MANAGER") // /manager로 들어오는 경우에는 ADMIN이나 MANAGER 권한 필요
+                        .requestMatchers("/admin/**").hasAnyRole("ADMIN") // /admin으로 들어오는 경우에는 ADMIN 권한 필요
+                        .anyRequest().permitAll() // 이외의 접근은 모두 허용)
+                )
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/loginForm") // 권한이 필요하다고 설정한 /user/**, /manager/**, /admin/**에 대해 /loginForm url로 이동하도록 설정
+                        /**
+                         * /login이 호출되면 security가 낚아채서 대신 로그인 프로세스 진행
+                         * 이렇게 하면 컨트롤러에 /login api를 안 만들어도 된다. security가 알아서 로그인을 진행해준다.
+                         */
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/") // 로그인 성공 시, redirect url 설정
+                );
         return http.build();
     }
 }
