d65a29d06bf1fa186703967f981a4620956a8dda
chocoding <galmeagi2@naver.com>
2025-03-13 22:26:05 +0900

    비밀번호 인코더 빈 등록

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,15 +13,20 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
+    public BCryptPasswordEncoder encodePwd() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(CsrfConfigurer::disable);
         http    .authorizeHttpRequests(auth -> auth
-                                .requestMatchers("/user/**").authenticated() // /user로 들어오는 경우에는 로그인을 해야 접근 가능
-                                .requestMatchers("/manager/**").hasAnyRole("ADMIN", "MANAGER") // /manager로 들어오는 경우에는 로그인 + ADMIN이나 MANAGER 권한이 있어야 접근 가능
-                                .requestMatchers("/admin/**").hasAnyRole("ADMIN") // /admin으로 들어오는 경우에는 로그인 + ADMIN 권한이 있어야 접근 가능
+                                .requestMatchers("/user/**").authenticated() // /user로 들어오는 경우에는 인증 필요
+                                .requestMatchers("/manager/**").hasAnyRole("ADMIN", "MANAGER") // /manager로 들어오는 경우에는 ADMIN이나 MANAGER 권한 필요
+                                .requestMatchers("/admin/**").hasAnyRole("ADMIN") // /admin으로 들어오는 경우에는 ADMIN 권한 필요
                                 .anyRequest().permitAll()) // 이외의 접근은 모두 허용)
                 .formLogin(formLogin -> // 권한이 필요하다고 설정한 /user/**, /manager/**, /admin/**에 대해 /login으로 이동하도록 설정
-                        formLogin.loginPage("/login"));
+                        formLogin.loginPage("/loginForm"));
 
         return http.build();
     }
