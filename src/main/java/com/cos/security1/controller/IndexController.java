bc87372746f953f6b092bca4cb093e0c4ef62962
chocoding <galmeagi2@naver.com>
2025-03-13 22:26:16 +0900

    회원가입 API 구현

--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -1,50 +1,74 @@
 package com.cos.security1.controller;
 
+import com.cos.security1.model.User;
+import com.cos.security1.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    private static final Logger log = LoggerFactory.getLogger(IndexController.class);
 
     @GetMapping({"", "/"})
     public String index() {
         return "index";
     }
 
-    @ResponseBody
     @GetMapping("/user")
     public String user() {
         return "user";
     }
 
-    @ResponseBody
     @GetMapping("/admin")
     public String admin() {
         return "admin";
     }
 
-    @ResponseBody
     @GetMapping("/manager")
     public String manager() {
         return "manager";
     }
 
-    @ResponseBody
-    @GetMapping("/login")
-    public String login() {
-        return "login";
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "loginForm";
     }
 
-    @ResponseBody
-    @GetMapping("/join")
-    public String join() {
-        return "join";
+    @GetMapping("/joinForm")
+    public String joinForm() {
+        return "joinForm";
     }
 
-    @ResponseBody
-    @GetMapping("/joinProc")
-    public String joinProc() {
-        return "회원가입 완료됨!";
+    @PostMapping("/join")
+    public String join(User user) {
+        user.setRole("ROLE_USER");
+//        userRepository.save(user);
+        /**
+         * 위처럼 그냥 단순히 spring data jpa를 사용해서 회원가입을 진행하면 security로 로그인할 수 없다.
+         * 이유는 패스워드가 암호화가 되지 않았기 때문
+         */
+
+        /**
+         * 암호화 진행
+         */
+        String rawPassword = user.getPassword();
+        // Bean으로 등록된 BCryptPasswordEncoder를 사용해서 인코딩 진행
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+
+        user.setPassword(encPassword);
+        userRepository.save(user);
+
+        return "redirect:/loginForm";
     }
 }
