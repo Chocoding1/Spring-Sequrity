08b1d0877da847a93fec700fc3ec015a5292c845
chocoding <galmeagi2@naver.com>
2025-03-14 16:45:23 +0900

    주석 추가

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -20,6 +20,12 @@ import org.springframework.security.web.SecurityFilterChain;
  */
 public class SecurityConfig {
 
+    /**
+     * BCryptPasswordEncoder
+     * 스프링 시큐리티(Spring Seurity) 프레임워크에서 제공하는 클래스 중 하나로 비밀번호를 암호화하는 데 사용할 수 있는 메서드를 가진 클래스
+     * PasswordEncoder 인터페이스를 구현한 클래스
+     * BCrypt 해싱 함수(BCrypt hashing function)를 사용해서 비밀번호를 인코딩해주는 메서드와 사용자의 의해 제출된 비밀번호와 저장소에 저장되어 있는 비밀번호의 일치 여부를 확인해주는 메서드를 제공
+     */
     @Bean
     public BCryptPasswordEncoder encodePwd() {
         return new BCryptPasswordEncoder();
